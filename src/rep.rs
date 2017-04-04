d0cfa1f6b19b1d59bc0a5b54caec360e9c3ddb5d
messense <messense@icloud.com>
2017-04-04 21:05:32 +0800

    Refine response deserialization

--- src/rep.rs
+++ src/rep.rs
@@ -1,9 +1,6 @@
-
-#[derive(Debug, Deserialize, Clone)]
-pub struct IpAddressSet {
-    #[serde(rename = "IpAddress")]
-    pub ip_address: Vec<String>,
-}
+use std::fmt;
+use std::marker::PhantomData;
+use serde::de::{self, Deserialize, Deserializer};
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Instance {
@@ -11,26 +8,62 @@ pub struct Instance {
     pub id: String,
     #[serde(rename = "InstanceName")]
     pub name: String,
+    #[serde(deserialize_with = "deserialize_single_key_map_to_vec")]
     #[serde(rename = "PublicIpAddress")]
-    pub public_ip_address: IpAddressSet,
+    pub public_ip_address: Vec<String>,
 }
 
-#[derive(Debug, Deserialize, Clone)]
-pub struct InstanceMap {
-    #[serde(rename = "Instance")]
-    pub instance: Vec<Instance>,
+impl Instance {
+    pub fn ip(&self) -> &str {
+        &self.public_ip_address[0]
+    }
 }
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Instances {
+    #[serde(deserialize_with = "deserialize_single_key_map_to_vec")]
     #[serde(rename = "Instances")]
-    pub instances: InstanceMap,
+    pub instances: Vec<Instance>,
     #[serde(rename = "TotalCount")]
     pub total: usize,
     #[serde(rename = "PageNumber")]
     pub page: usize,
     #[serde(rename = "PageSize")]
     pub size: usize,
+}
+
+fn deserialize_single_key_map_to_vec<V, D>(d: D) -> Result<Vec<V>, D::Error>
+    where D: Deserializer,
+          V: Deserialize
+{
+    struct SingleKeyMapToVecVisitor<V: Deserialize>(PhantomData<V>);
+
+    impl<V> de::Visitor for SingleKeyMapToVecVisitor<V>
+        where V: Deserialize
+    {
+        type Value = Vec<V>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a map")
+        }
+
+        #[inline]
+        fn visit_unit<E>(self) -> Result<Vec<V>, E> {
+            Ok(Vec::new())
+        }
+
+        #[inline]
+        fn visit_map<T>(self, mut visitor: T) -> Result<Vec<V>, T::Error>
+            where T: de::MapVisitor
+        {
+            let item: Option<(String, Vec<V>)> = visitor.visit()?;
+            if let Some((_, value)) = item {
+                return Ok(value);
+            }
+            Ok(Vec::new())
+        }
+    }
+    d.deserialize_map(SingleKeyMapToVecVisitor(PhantomData))
 }
 
 #[cfg(test)]
@@ -96,6 +129,9 @@ mod tests {
         assert_eq!(instances.page, 1);
         assert_eq!(instances.size, 10);
         assert_eq!(instances.total, 1);
-        assert_eq!(instances.instances.instance.len(), 1);
+        assert_eq!(instances.instances.len(), 1);
+        let instance = &instances.instances[0];
+        assert_eq!(instance.id, "i-94t3s0jxk");
+        assert_eq!(instance.ip(), "120.25.13.106");
     }
 }
